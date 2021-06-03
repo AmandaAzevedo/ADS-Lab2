f0028a70b59b437fa588be4c69aa9d3e8fd5f11b
Amanda Azevedo <amanda.azevedo@dcx.ufpb.br>
2021-06-03 12:51:34 -0300

    Script R para plotar gráficos da atividade finalizado

--- graphics.r
+++ graphics.r
@@ -1,20 +1,35 @@
 library(dplyr)
 library(ggplot2)
 
-question2 <- read.csv("/Users/Amanda/Downloads/ADS-Lab2/ADS-Lab2-master/out/out-03_33_40-2021-06-03/atv2-results-tcm.csv")
+# Question 2 answer chart
+question2 <- read.csv("/Users/Amanda/Documents/Repositorios/ADS-Lab2/out/out-03_33_40-2021-06-03/atv2-results-tcm.csv")
 ggplot(question2) + 
   aes (x = TaxaDeChegadaMedia, y = TempoMedioDeResposta) + 
   geom_line(col="#030CE6", lwd=1) +
-  labs(x="Taxa de chegada média",
-       y="Tempo médio de resposta",
-       title = "Questão 2. Variação do tempo de resposta")
+  labs(x="Average arrival rate",
+       y="Average response time",
+       title = "Question 2. Response time variation by arrival rate")
 
 
-
-question3 <- read.csv("/Users/Amanda/Downloads/ADS-Lab2/ADS-Lab2-master/out/out-03_47_23-2021-06-03/atv2-results-numS.csv")
+# Question 3 answer chart
+question3 <- read.csv("/Users/Amanda/Documents/Repositorios/ADS-Lab2/out/out-15_09_38-2021-06-03/atv2-results-numS.csv")
 ggplot(question3) + 
   aes (x = NumServidores, y = TempoMedioDeResposta) + 
   geom_line(col="#030CE6", lwd=1) +
-  labs(x="Número de servidores",
-       y="Tempo médio de resposta",
-       title = "Questão 3. Variação do número de servidores por tempo médio de resposta")
+  labs(x="Number of servers",
+       y="Average response time",
+       title = "Question 3. Response time variation by number of servers")
+
+
+
+
+# Question 4 answer chart
+question4 <- read.csv("/Users/Amanda/Documents/Repositorios/ADS-Lab2/out/out-12_28_21-2021-06-03/atv2-results-tcm.csv")
+ggplot(question4) + 
+  aes (x = TaxaDeChegadaMedia, y = TempoMedioDeResposta) + 
+  geom_line(col="#030CE6", lwd=1) +
+  labs(x="TaxaDeChegadaMedia",
+       y="TempoMedioDeResposta",
+       title = "Question 4. Average response time per arrival rate")
+
+
